2752b96df348e0cb213e040778c5e24d4c8be68b
Richard Nguyen <81831944+richardn257@users.noreply.github.com>
2023-07-02 17:18:05 -0700

    Add a way for users to change size of grid
    Reorganize the node selectors by creating a function. This function
    has a parameter for the number of squares which will be used to create
    the grid.

--- javascript.js
+++ javascript.js
@@ -1,17 +1,32 @@
-const container = document.querySelector('.container');
-
-for (let i = 0; i < 16; i++) {
-  const row = document.createElement('div');
-  row.classList.add('rowOfSquares');
-  container.appendChild(row);
-}
-
-const rows = document.querySelectorAll('.rowOfSquares');
-rows.forEach(row => {
-  for (let i = 0; i < 16; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    row.appendChild(square);
+const gridButton = document.querySelector('button');
+gridButton.addEventListener('click', () => {
+  userInput = prompt('Enter the number of squares per sides for the grid', 16);
+  while (userInput > 100) {
+    userInput = prompt('The maximum is 100. Enter again')
   }
+
 });
 
+function createGrid(squares) {
+  const container = document.querySelector('.container');
+
+  for (let i = 0; i < squares; i++) {
+    const row = document.createElement('div');
+    row.classList.add('rowOfSquares');
+    container.appendChild(row);
+  }
+
+  const rows = document.querySelectorAll('.rowOfSquares');
+  rows.forEach(row => {
+    for (let i = 0; i < squares; i++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      square.addEventListener('mouseover', () => {
+        square.classList.add('color-trail')
+      });
+      row.appendChild(square);
+    }
+  });
+}
+
+createGrid(16);
